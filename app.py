811b2a8afe677541984e1496c987a4226c262463
Maddison Meier <maddisonmeier@Maddisons-MacBook-Pro.local>
2016-12-24 22:50:34 -0700

    inital run

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
+from email.mime.text import MIMEText
+
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
@@ -13,6 +16,31 @@ def past():
 @app.route('/future')
 def future():
 	return render_template('future.html')
+
+
+EMAIL_MESSAGE = "Message from iamsonez.com\n \
+				 name: {0}\n \
+				 email: {1}\n \
+				 message: {2}"
+
+ME = 'mmeier@eng.ucsd.edu'
+SONEZ = 'iamsonez.com'
+
+@app.route('/email', methods=['POST'])
+def send_email():
+	msg = EMAIL_MESSAGE.format(request.form.get("name"), 
+							   request.form.get("email"), 
+							   request.form.get("message"))
+
+	msg = MIMEText(msg)
+	msg['Subject'] = 'New Inquiry From {0}'.format(request.form.get("name"))
+	msg['To'] = ME
+	msg['From'] = SONEZ
+
+	s = smtplib.SMTP('smtpout.secureserver.net')
+	s.sendmail(SONEZ, ME, msg.as_string())
+	s.quit()
+
 
 if __name__ == '__main__':
 	app.run()
